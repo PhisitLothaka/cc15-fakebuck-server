93e0a6728df829a06d3da60b656fd5ab3cb8c84f
PhisitLothaka <tenphisit1998@gmail.com>
2023-09-28 17:27:18 +0700

    register success

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { registerSchema } = require("../validators/auth-validator");
+const prisma = require("../models/prisma");
+
+exports.register = async (req, res, next) => {
+  try {
+    const { value, error } = registerSchema.validate(req.body);
+
+    if (error) {
+      return next(error);
+    }
+    value.password = await bcrypt.hash(value.password, 12);
+    const user = await prisma.user.create({
+      data: value,
+    });
+
+    const payload = { userId: user.id };
+    const accessToken = jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY || "1234wesdf2345wdff",
+      {
+        expiresIn: process.env.JWT_EXPIRE,
+      }
+    );
+
+    res.status(201).json({ accessToken });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    // const result = loginSchema.validate(req.body)
+  } catch (err) {
+    next(err);
+  }
+};
